0b6a7f1573985337e770881e8883626d9288cb5f
Johan GIRARD <magicjo@tiglab.net>
2019-06-22 23:36:17 +0200

    Make example

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+[![Build Status](https://travis-ci.org/magicjo/learn-rxjs-operator-take-until.svg?branch=master)](https://travis-ci.org/magicjo/learn-rxjs-operator-take-until)
+
 # learn-rxjs-operator-take-until
 
+Example about the filtering operator `takeUntil`.
+
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/learn-rxjs-operator-take-until)
+
+## How to execute in local ?
+
+Execute `src/index.ts`:
+```bash
+docker run -v $(pwd):/app/src magicjo/learn-rxjs:latest start
+```
+
+Execute `tslint`:
+```bash
+docker run -v $(pwd):/app/src magicjo/learn-rxjs:latest lint
+```
--- index.ts
+++ index.ts
@@ -1,9 +1,32 @@
-import { of } from 'rxjs'; 
-import { map } from 'rxjs/operators';
+// RxJS v6+
+import { interval, timer } from "rxjs";
+import { takeUntil } from "rxjs/operators";
 
+/*
+ Filtering Operator: takeUntil
+ -----------------------------
 
-const source = of('World').pipe(
-  map(x => `Hello ${x}!`)
-);
+ Lets values pass until a second Observable, **notifier**, emits something. Then, it completes.
+ ```
+ takeUntil(notifier: Observable): Observable
+ ```
 
-source.subscribe(x => console.log(x));
+ http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#instance-method-takeUntil
+*/
+
+// Emit: 0, 1, 2, ... (every 1s)
+const source = interval(1000);
+
+// After 5 seconds, emit value
+const timer$ = timer(5000); // or interval(5000);
+
+// When timer emits after 5s, complete source
+const example = source.pipe(takeUntil(timer$));
+
+// Receive: 0, 1, 2, 3
+console.log("[start]");
+example.subscribe({
+  complete: () => console.log("[complete]"),
+  error: err => console.error("[error] : ", err),
+  next: value => console.log("[next] : ", value)
+});
